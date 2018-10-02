c8ec3880a0625558bafb06354059ca8da7e3ba47
Josue Alvarez <josuetec2003@gmail.com>
2018-10-02 11:23:14 -0600

    v3: opciones agregadas

--- super_calc.cpp
+++ super_calc.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main (int argc, char const *argv[])
 {
+	int o;
+
 	cout << "Bienvenido a la super calculadora" << endl;
+
+	cout << "Ingrese la opcion que desea ejecutar" << endl;
+	cout << "1- Sumar" << endl;
+	cout << "2- Restar" << endl;
+	cout << "3- Dividir"<< endl;
+	cout << "4- Multiplicar " << endl;
+	cin >> o;
+
 	return 0;
 }
